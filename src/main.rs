9462a96009b97f7dfa1859e04ff58974640596a3
Linus Kirkwood <linuskirkwood@gmail.com>
2025-11-23 23:08:23 +1100

    Update cli to include random program generation

--- src/main.rs
+++ src/main.rs
@@ -4,27 +4,24 @@ use std::{
 };
 
 use anyhow::Result;
+use randprog::random_program;
 
 #[macro_use]
 mod ast;
 mod interpreter;
 mod parser;
+mod randprog;
 mod tokeniser;
 
 fn help() {
     eprintln!("minilisp - a tiny LISP interpreter.");
-    eprintln!("Usage: minilisp [--help | -h] [--repl | -r] [FILE]\n");
-    eprintln!("If FILE is '-', the program will be read from stdin.\n");
-    eprintln!("Options:");
-    eprintln!("\t --help | -h    --   Print this message and exit.");
-    eprintln!("\t --repl | -r    --   Enter a primitive REPL.");
-}
-
-fn repl() -> Result<String> {
-    let mut program = String::new();
-    stdin().lock().read_line(&mut program)?;
-    println!("{:-<80}", "");
-    Ok(program)
+    eprintln!("Usage: minilisp [run FILE | repl | random | help] \n");
+    eprintln!("Commands:");
+    eprintln!("\t run [FILE]     --   Run a program at FILE.");
+    eprintln!("\t                     If FILE is '-', the program will be read from stdin.\n");
+    eprintln!("\t repl           --   Enter a primitive read-eval-print environment.");
+    eprintln!("\t random         --   Generate a random minilisp program.");
+    eprintln!("\t help           --   Print this message and exit.");
 }
 
 fn run(program: &str) -> Result<()> {
@@ -39,6 +36,13 @@ fn run(program: &str) -> Result<()> {
     Ok(())
 }
 
+fn repl() -> Result<String> {
+    let mut program = String::new();
+    stdin().lock().read_line(&mut program)?;
+    println!("{:-<80}", "");
+    Ok(program)
+}
+
 fn main() -> Result<()> {
     let mut args = std::env::args();
     match args.nth(1) {
@@ -50,21 +54,35 @@ fn main() -> Result<()> {
         Some(arg) => {
             let arg = arg.as_str();
 
-            if matches!(arg, "--help" | "-h") {
+            if arg == "help" {
                 help();
-            } else if matches!(arg, "--repl" | "-r") {
-                run(&repl()?)?;
-            } else if arg == "-" {
-                run(args.collect::<String>().as_str())?;
-            } else {
-                match std::fs::read_to_string(arg) {
-                    Ok(program) => run(&program)?,
-                    Err(err) => {
-                        eprintln!("Failed to read program at {arg}: {err}\n\n");
+            } else if arg == "run" {
+                let file = match args.next() {
+                    None => {
+                        eprintln!("The run command requires a file as an argument.");
                         help();
                         exit(1);
                     }
+                    Some(file) => file,
+                };
+
+                if file == "-" {
+                    run(args.collect::<String>().as_str())?;
+                } else {
+                    match std::fs::read_to_string(&file) {
+                        Ok(program) => run(&program)?,
+                        Err(err) => {
+                            eprintln!("Failed to read program at {file}: {err}\n\n");
+                            help();
+                            exit(1);
+                        }
+                    }
                 }
+            } else if arg == "repl" {
+                run(&repl()?)?;
+            } else if arg == "random" {
+                let program_expr = random_program()?;
+                todo!("unparse and untokenise {program_expr:#?}");
             }
         }
     }
